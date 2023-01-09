e3cb446bcab710fbbd265c7de9643b045cb6576d
Amanda Stern <amanda.stern.73@gmail.com>
2023-01-09 13:59:12 -0500

    update app.js error handling, incorporate Joi error validation

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require ('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
+const Joi = require('joi')
+const catchAsync = require('./utilities/catchAsync')
+const ExpressError = require('./utilities/ExpressError')
 const methodOverride = require('method-override')
 const Campground = require('./models/campground')
 
@@ -26,47 +29,64 @@ app.get('/', (req, res) => {
   res.render('home')
 })
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
   const campgrounds = await Campground.find({})
   res.render('campgrounds/index', { campgrounds })
-})
+}))
 
 app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', catchAsync(async (req, res, next) => {
+  // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
+  const campgroundSchema = Joi.object({
+    campground: Joi.object({
+      title: Joi.string().required,
+      price: Joi.number.required().min(0)
+    }).required()
+  })
+  const result = campgroundSchema.validate(req.body)
+  console.log('this is result:', result)
+  if (result.error){
+    throw new ExpressError(result.error.details, 400)
+  }
   const campground = new Campground(req.body.campground)
   await campground.save()
   res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id)
   res.render('campgrounds/show', { campground })
-})
+}))
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id)
   res.render('campgrounds/edit', { campground })
-})
+}))
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params
   const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
   res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params
   const campground = await Campground.findByIdAndDelete(id)
   res.redirect('/campgrounds')
+}))
+
+app.all('*', (req, res, next) => {
+  next(new ExpressError('Page not found!', 404))
 })
 
 // 404 & other handling middleware at the end of the file if routes are not found
 app.use((error, req, res, next) => {
-  res.status(404).send('Errrrrrrorrrrr')
-  next(error)
+  const { statusCode = 500 } = error
+  if(!error.message) error.message = 'Oh no, something went wrong!'
+  res.status(statusCode).render('error', { error })
 })
 
 app.listen(3000, () => {

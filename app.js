f7bbfadf2ed15cf943986ab57608c1c112762681
Amanda Stern <amanda.stern.73@gmail.com>
2023-01-09 15:16:35 -0500

    update validatoin on server-side

--- app.js
+++ app.js
@@ -43,13 +43,18 @@ app.post('/campgrounds', catchAsync(async (req, res, next) => {
   const campgroundSchema = Joi.object({
     campground: Joi.object({
       title: Joi.string().required,
-      price: Joi.number.required().min(0)
+      price: Joi.number.required().min(0),
+      image: Joi.string().required,
+      location: Joi.string.required(),
+      description: Joi.string.required()
     }).required()
   })
-  const result = campgroundSchema.validate(req.body)
+  const { error } = campgroundSchema.validate(req.body)
   console.log('this is result:', result)
-  if (result.error){
-    throw new ExpressError(result.error.details, 400)
+  if (error){
+    const msg = error.details.map(el =>
+      el.message).join(',')
+      throw new ExpressError(msg, 400)
   }
   const campground = new Campground(req.body.campground)
   await campground.save()

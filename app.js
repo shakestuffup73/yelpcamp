6d6cfa10ffa916b41e69fe52d6a0c571718fb2f5
Amanda Stern <amanda.stern.73@gmail.com>
2023-01-24 13:43:45 -0500

    update app with sessionConfig

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const express = require ('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
+const session = require('express-session')
 const ExpressError = require('./utilities/ExpressError')
 const methodOverride = require('method-override')
 
 const campgrounds = require('./routes/campgrounds')
 const reviews = require('./routes/reviews')
 
-mongoose.connect('mongodb://localhost:27017/yelpcamp')
+mongoose.connect('mongodb://localhost:27017/yelpcamp', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+})
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
@@ -24,7 +30,19 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(express.static(path.join(__dirname, 'public')))
 
+const sessionConfig = {
+  secret: 'thisshouldbeabettersecret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  }
+}
+app.use(session(sessionConfig))
 
 // MOUNTED ROUTERS
 app.use('/campgrounds', campgrounds)

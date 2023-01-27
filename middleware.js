134a2595a894c85ab52a4c111d4213c17ed1b0b3
Amanda Stern <amanda.stern.73@gmail.com>
2023-01-27 13:16:37 -0500

    refactor and move middleware

--- middleware.js
+++ middleware.js
@@ -1,3 +1,7 @@
+const { campgroundSchema } = require('../schemas.js');
+const ExpressError = require('./utils/ExpressError')
+const Campground = require('../models/campground')
+
 module.exports.isLoggedIn = (req, res, next) => {
   console.log('This is req.user:', req.user)
   if (!req.isAuthenticated()) {
@@ -10,3 +14,33 @@ module.exports.isLoggedIn = (req, res, next) => {
   next()
 }
 
+module.exports.validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+      const msg = error.details.map(el => el.message).join(',')
+      throw new ExpressError(msg, 400)
+  } else {
+      next();
+  }
+}
+
+module.exports.isAuthor = async (req, res, next) => {
+  const { id } = req.params
+  const campground = await Campground.findById(id)
+  if (!campground.author.equals(req.user._id)) {
+      req.flash('error', 'You do not have permission to do that.')
+      return res.redirect(`/campgrounds/${id}`)
+  }
+  next()
+}
+
+module.exports.validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body)
+  if (error) {
+    const msg = error.details.map(el =>
+      el.message).join(',')
+    throw new ExpressError(msg, 400)
+  } else {
+    next()
+  }
+}
--- routes/reviews.js
+++ routes/reviews.js
@@ -13,16 +13,8 @@ const catchAsync = require('../utils/catchAsync')
 const ExpressError = require('../utils/ExpressError')
 
 // MIDDLEWARE
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body)
-  if (error) {
-    const msg = error.details.map(el =>
-      el.message).join(',')
-    throw new ExpressError(msg, 400)
-  } else {
-    next()
-  }
-}
+const { validateReview } = require('../middleware')
+
 
 router.post('/', validateReview, catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id)

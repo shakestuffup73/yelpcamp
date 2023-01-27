7318563556dbf64601b971f422c40d55ed1da634
Amanda Stern <amanda.stern.73@gmail.com>
2023-01-27 16:56:09 -0500

    refactor and chain routes

---
+++ controllers/users.js
@@ -0,0 +1,46 @@
+const User = require('../models/user')
+
+module.exports.renderRegister = (req, res) => {
+  res.render('users/register')
+}
+
+module.exports.register = async (req, res) => {
+  try {
+    const { email, username, password } = req.body
+    const user = new User({email, username})
+    const registeredUser = await User.register(user, password)
+    console.log('this is registeredUser:', registeredUser)
+    req.login(registeredUser, error =>{
+      if (error) return next(error)
+      req.flash('success', 'Welcome to Yelp Camp!')
+      res.redirect('/campgrounds')
+    })
+  } catch (error) {
+    req.flash('error', error.message)
+    res.redirect('/register')
+  }
+}
+
+module.exports.renderLogin = (req, res) => {
+  res.render('users/login')
+}
+
+module.exports.login = (req, res) => {
+  req.flash('success', 'welcome back')
+  // if there's a redirectURL, it will go to that, if not, it will default to redirecting to '/campgrounds'
+  const redirectURL = req.session.returnTo || '/campgrounds'
+  console.log('this is redirectURL', redirectURL)
+  console.log('this is req.session.returnTo', req.session.returnTo)
+  res.redirect(redirectURL)
+  delete req.session.returnTo
+}
+
+module.exports.logout = (req, res) => {
+  req.logout(function(error){
+    if (error) {
+      return next(error)
+    }
+    req.flash('success', 'Goodbye!')
+    res.redirect('/campgrounds')
+  })
+} 
--- routes/users.js
+++ routes/users.js
@@ -2,50 +2,16 @@ const express = require('express')
 const router = express.Router()
 const passport = require('passport')
 const User = require('../models/user')
+const users = require('../controllers/users')
 
-router.get('/register', (req, res) => {
-  res.render('users/register')
-})
+router.route('/register')
+  .get('/register', users.renderRegister)
+  .post('/register', catchAsync(users.register))
 
-router.post('/register', async(req, res) => {
-  try {
-    const { email, username, password } = req.body
-    const user = new User({email, username})
-    const registeredUser = await User.register(user, password)
-    console.log('this is registeredUser:', registeredUser)
-    req.login(registeredUser, error =>{
-      if (error) return next(error)
-      req.flash('success', 'Welcome to Yelp Camp!')
-      res.redirect('/campgrounds')
-    })
-  } catch (error) {
-    req.flash('error', error.message)
-    res.redirect('/register')
-  }
-})
+router.route('/login')
+  .get('/login', users.renderLogin)
+  .post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo: true }), users.login)
 
-router.get('/login', (req, res) => {
-  res.render('users/login')
-})
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo: true }), (req, res) => {
-  req.flash('success', 'welcome back')
-  // if there's a redirectURL, it will go to that, if not, it will default to redirecting to '/campgrounds'
-  const redirectURL = req.session.returnTo || '/campgrounds'
-  console.log('this is redirectURL', redirectURL)
-  console.log('this is req.session.returnTo', req.session.returnTo)
-  res.redirect(redirectURL)
-  delete req.session.returnTo
-})
-
-router.get('/logout', (req, res) => {
-  req.logout(function(error){
-    if (error) {
-      return next(error)
-    }
-    req.flash('success', 'Goodbye!')
-    res.redirect('/campgrounds')
-  })
-})
+router.get('/logout', users.logout)
 
 module.exports = router

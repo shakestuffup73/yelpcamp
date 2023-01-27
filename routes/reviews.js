d2116a404d9428b7c802b8227085df5768df8350
Amanda Stern <amanda.stern.73@gmail.com>
2023-01-27 16:36:38 -0500

    refactor and add controllersdirectory and controller functions moved to file

--- routes/reviews.js
+++ routes/reviews.js
@@ -13,7 +13,7 @@ const catchAsync = require('../utils/catchAsync')
 const ExpressError = require('../utils/ExpressError')
 
 // MIDDLEWARE
-const { validateReview, isLoggedIn } = require('../middleware')
+const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware')
 
 
 router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
@@ -27,7 +27,7 @@ router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
   res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-router.delete('/:reviewId', catchAsync(async (req, res) => {
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
   const { id, reviewId } = req.params
   await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
   await Review.findByIdAndDelete(reviewId)

5f24849939061fe520ba86239671db4ccfacd105
Amanda Stern <amanda.stern.73@gmail.com>
2023-01-28 11:44:47 -0500

    unchain routes because it was causing bugs

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,13 @@ const User = require('../models/user')
 const users = require('../controllers/users')
 const catchAsync = require('../utils/catchAsync')
 
-router.route('/register')
-  .get('/register', users.renderRegister)
-  .post('/register', catchAsync(users.register))
-
-router.route('/login')
-  .get('/login', users.renderLogin)
-  .post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo: true }), users.login)
+router.get('/register', users.renderRegister)
+  
+router.post('/register', catchAsync(users.register))
+  
+router.get('/login', users.renderLogin)
+  
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo: true }), users.login)
 
 router.get('/logout', users.logout)
 

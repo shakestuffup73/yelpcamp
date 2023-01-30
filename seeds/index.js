ec3ee5b94d9d7b63b3a5c8d8319a91c0fc089f79
Amanda Stern <amanda.stern.73@gmail.com>
2023-01-30 10:55:31 -0500

    update seeds file with cloudinary image links

--- seeds/index.js
+++ seeds/index.js
@@ -22,7 +22,16 @@ const seedDB = async () => {
       author: '63d2b28f230b7269cb0921bb',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: 'https://source.unsplash.com/collection/483251/350x250',
+      images: [
+        {
+          url: 'https://res.cloudinary.com/dgwexf22j/image/upload/v1674928671/YelpCamp/ivysjmewroyooqour9sq.jpg',
+          filename: 'YelpCamp/ivysjmewroyooqour9sq'
+        },
+        {
+          url: 'https://res.cloudinary.com/dgwexf22j/image/upload/v1674928671/YelpCamp/u8mvukkftih4uojl5rwy.jpg',
+          fileneame: 'YelpCamp/u8mvukkftih4uojl5rwy',
+        }
+      ],
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, libero eos expedita deleniti aperiam assumenda ipsa possimus odit earum numquam. Saepe placeat ea repellat adipisci. Unde debitis laudantium optio corrupti.',
       price: price,
     })

427f51a819a7c60f3d8e973dd205b74bc3694e01
Amanda Stern <amanda.stern.73@gmail.com>
2023-02-01 16:56:39 -0500

    update app.js

--- app.js
+++ app.js
@@ -31,6 +31,16 @@ mongoose.connect(dbURL)
 // Local Database Connection
 //'mongodb://localhost:27017/yelpcamp'
 
+main().catch((err) => console.log(err));
+async function main() {
+	mongoose.connect(dbURL);
+	console.log("Connection open!");
+	// Starting up app on desired port
+	app.listen(PORT, () => {
+		console.log(`Listening on http://localhost:${PORT}`);
+	});
+}
+
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => {
@@ -139,8 +149,4 @@ app.use((error, req, res, next) => {
   const { statusCode = 500 } = error
   if (!error.message) error.message = 'Oh no, something went wrong!'
   res.status(statusCode).render('error', { error })
-})
-
-app.listen(PORT, () => {
-  console.log(`Listening on Port ${ PORT }!`)
 })
